493ec7656bc866e6dc7bcd40bb1ead5bb8d14aa4
Mokshith Chilukuri <moksha@MacBook-Air.local>
2023-02-10 10:27:12 +0530

    new

---
+++ demo.cpp
@@ -0,0 +1,30 @@
+/*
+Mokshith Ch  |,,|
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define debug(x) cout<<#x<<": "<<x<<endl;
+#define google cout<<"Case #" << tc << ": ";
+ 
+void solve(int tc)
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+}
+ 
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t=1; 
+    cin>>t;
+    for(int tc=1;tc<=t;tc++) solve(tc);
+    return 0;
+}
